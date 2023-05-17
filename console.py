4945076f7fc9089a320ea765ea28309ff2e8bc58
Dennis Muvaa <dmuvaa70@gmail.com>
2023-05-17 15:14:18 -0700

    Console Changes

--- console.py
+++ console.py
@@ -144,20 +144,20 @@ class HBNBCommand(cmd.Cmd):
                     print("** instance id missing **")
             elif command.startswith("update(") and command.endswith(")"):
                 update_args = command[7:-1].split(",")  # Split
-                if len(update_args) == 3:
+                if len(update_args) == 2:
                     id_str = update_args[0].strip()
-                    attribute_name = update_args[1].strip()
-                    attribute_value = update_args[2].strip()
+                    try:
+                        attribute_dict = eval(update_args[1].strip())
+                    except (SyntaxError, NameError):
+                        print("** Invalid dictionary representation **")
+                        return
                     if id_str:
-                        command_str = (
-                                args[0] + " " +
-                                id_str + " " +
-                                attribute_name + " " +
-                                attribute_value
-                        )
-                        self.do_update(command_str)
+                        command_str = args[0] + " " + id_str
+                        self.do_update_dict(command_str, attribute_dict)
                     else:
                         print("** instance id missing **")
+                else:
+                    print("** Invalid update format **")
             else:
                 print("** Unknown command **")
         else:
